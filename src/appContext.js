de268552c3b8e523d7decfbb6b8fd77f838b764b
paradigmzero <josephrcalton@gmail.com>
2023-03-10 11:41:03 +0000

    Allows the aws-sdk alongside the more legacy libraries from tweaks in libraries but getting Parsing error issues in the code saying to use require and not imports... Still runs though

--- src/appContext.js
+++ src/appContext.js
@@ -1,16 +1,22 @@
 import React, { createContext, useContext, useReducer, useState } from "react";
 import { reducer } from "./libs/reducer";
 import { initialState } from "./libs/initialState";
+import AWS from 'aws-sdk'
+
 
 const AppContext = createContext(null);
 
 export function AppProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [refreshSwitch, setRefreshSwitch] = useState(true);
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+  });
 
   return (
     <AppContext.Provider
-      value={{ state, dispatch, refreshSwitch, setRefreshSwitch }}
+      value={{ state, dispatch, refreshSwitch, setRefreshSwitch, s3 }}
     >
       {children}
     </AppContext.Provider>
